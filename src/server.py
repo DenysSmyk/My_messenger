bb2cd5da40c92dbc2027128cb0d6d37886285ef9
Denys Smyk <smukdenys10@gmail.com>
2019-10-15 19:07:21 +0300

    connected and disconnected clients

--- src/server.py
+++ src/server.py
@@ -1,15 +1,27 @@
 from twisted.internet import reactor
-from twisted.internet.protocol import ServerFactory
+from twisted.internet.protocol import ServerFactory, connectionDone
 from twisted.protocols.basic import LineOnlyReceiver
 
 
 class Handler(LineOnlyReceiver):
+    factory: 'Server'
+
+    def connectionLost(self, reason=connectionDone):
+        print("Disconnected")
+
+    def connectionMade(self):
+        print("Connected")
+
     def lineReceived(self, line):
-        print(line)
+        print(f"Message: {line}")
 
 
 class Server(ServerFactory):
     protocol = Handler
+    clients: list
+
+    def __init__(self):
+        self.clients = []
 
     def startFactory(self):
         print("Server started...")
