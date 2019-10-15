027ecf5aacf958ab087cb4d48a3491cb3b67cebc
Denys Smyk <smukdenys10@gmail.com>
2019-10-15 19:34:07 +0300

    added user registration

--- src/server.py
+++ src/server.py
@@ -5,16 +5,35 @@ from twisted.protocols.basic import LineOnlyReceiver
 
 class Handler(LineOnlyReceiver):
     factory: 'Server'
+    login: str
 
     def connectionLost(self, reason=connectionDone):
+        self.factory.clients.remove(self)
         print("Disconnected")
 
     def connectionMade(self):
+        self.login = None
+        self.factory.clients.append(self)
         print("Connected")
 
-    def lineReceived(self, line):
-        print(f"Message: {line}")
+    def lineReceived(self, line: bytes):
+        message = line.decode()
 
+        if self.login is not None:
+            message = f"<{self.login}>: {message}"
+
+            for user in self.factory.clients:
+                if user is not self:
+                    user.sendLine(message.encode())
+        else:
+            if message.startswith("login:"):
+                login = message.replace("login:", "")
+
+                self.login = login
+                print(f"New user: {login}")
+                self.sendLine("Welcome!!!".encode())
+            else:
+                self.sendLine("Incorrect login".encode())
 
 class Server(ServerFactory):
     protocol = Handler
